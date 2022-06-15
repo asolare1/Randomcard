abee61eb424184f6beb2274c00cfb23a62cf212d
asolare1 <asolare1@gmail.com>
2022-06-15 05:11:51 +0000

    me

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector('.card').innerHTML = generaterandomsuit();
+  document.querySelector('.numbers').innerHTML = generaterandomnumber();
+
 };
+  let generaterandomsuit = () => {
+  let suit = ["♦","♥","♠","♣"];
+  let indexsuits = Math.floor(Math.random() * suit.length);
+  
+  return suit[indexsuits];
+
+};
+let generaterandomnumber = () => {
+  let numbers = ["A","2","3","4","6","7","8","9","J","Q","K"];
+  let indexnumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexnumbers];
+};
+
