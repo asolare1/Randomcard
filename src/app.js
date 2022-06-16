7f4b359eb9182e6dfd6e3eaa49cc954ed5bac153
asolare1 <asolare1@gmail.com>
2022-06-16 04:44:33 +0000

    me

--- src/app.js
+++ src/app.js
@@ -4,17 +4,27 @@ import "./style.css";
 
 
 window.onload = () => {
-  document.querySelector('.card').innerHTML = generaterandomsuit();
+  document.querySelector('.suits').innerHTML = generaterandomsuit();
   document.querySelector('.numbers').innerHTML = generaterandomnumber();
+  document.querySelector('.suits2').innerHTML = document.querySelector('.suits').innerHTML = generaterandomsuit();
+  
+  
+
 
 };
   let generaterandomsuit = () => {
-  let suit = ["♦","♥","♠","♣"];
-  let indexsuits = Math.floor(Math.random() * suit.length);
-  
-  return suit[indexsuits];
 
-};
+  let suits = ["<p>♦️</p>","<p>♥</p>","♠","♣"];
+
+  let indexsuits = Math.floor(Math.random() * suits.length);
+
+  return suits[indexsuits];
+
+  
+  };
+
+
+
 let generaterandomnumber = () => {
   let numbers = ["A","2","3","4","6","7","8","9","J","Q","K"];
   let indexnumbers = Math.floor(Math.random() * numbers.length);
